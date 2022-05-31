717bb6794b7c83c8794858c432dcd0114a91229d
artsemy <artemiy.konoplyanik@gmail.com>
2022-05-31 15:25:12 +0300

    p1.3fix

--- P1/p1.3.rkt
+++ P1/p1.3.rkt
@@ -1,8 +1,12 @@
 #lang racket
 (define (square x) (* x x))
 (define (sum-of-squares x y) (+ (square x) (square y)))
+(define (min-of-three a b c)
+  (cond ((and (<= a b) (<= a c)) a)
+        ((and (<= b a) (<= b c)) b)
+        (else c)))
 (define (sum-square-two-max a b c) (cond
-                                     ((= (min a b c) a) (sum-of-squares b c))
-                                     ((= (min a b c) b) (sum-of-squares a c))
-                                     ((= (min a b c) c) (sum-of-squares a b))))
+                                     ((= (min-of-three a b c) a) (sum-of-squares b c))
+                                     ((= (min-of-three a b c) b) (sum-of-squares a c))
+                                     ((= (min-of-three a b c) c) (sum-of-squares a b))))
 (sum-square-two-max 3 5 5)
