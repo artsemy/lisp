fcf017f83bcbbeffe2b19dab1b729182dc39ac5c
artsemy <artemiy.konoplyanik@gmail.com>
2022-06-07 16:16:21 +0300

    p1.36

---
+++ P1/p1.36.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess attempt)
+    (display attempt) (display " - ") (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next (+ attempt 1)))))
+  (try first-guess 1))
+
+(define (average x y) (/ (+ x y) 2))
+
+(fixed-point (lambda (y) (/ (log 1000) (log y))) 2.0)
+
+(fixed-point (lambda (y) (average y (/ (log 1000) (log y)))) 2.0)
